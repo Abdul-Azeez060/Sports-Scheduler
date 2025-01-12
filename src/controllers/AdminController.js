f91c9bc623e6a2732dea9b3c17ce19c7dde3af35
Abdul-Azeez060 <azeez160604@gmail.com>
2025-01-12 12:13:23 +0530

    add the edit feature

--- src/controllers/AdminController.js
+++ src/controllers/AdminController.js
@@ -20,7 +20,71 @@ class AdminController {
       },
     });
 
+    console.log(sports, "this is the sports");
+
     res.render("admin/dashboard", { user: req.user, sports });
+  }
+
+  static async updateSportsController(req, res) {
+    if (req.user.role != "ADMIN") {
+      return res.redirect("/player/dashboard");
+    }
+    const id = req.params.id;
+
+    try {
+      const { title, description, matchDate, location, totalSpots } = req.body;
+
+      const { error } = SportSchema.safeParse({
+        title,
+        description,
+        matchDate: new Date(matchDate),
+        location,
+        totalSpots: parseInt(totalSpots),
+      });
+      if (error) {
+        console.log(error.issues[0]);
+        return res.render("admin/create-sport", {
+          user: req.user,
+          error: error.issues[0].message + " " + error.issues[0].path,
+        });
+      }
+      await prisma.sport.update({
+        where: {
+          id,
+        },
+        data: {
+          title,
+          description,
+          matchDate: new Date(matchDate),
+          location,
+          totalSpots: parseInt(totalSpots),
+          availableSpots: parseInt(totalSpots),
+          adminId: req.user.id,
+        },
+      });
+      res.redirect("/admin/dashboard");
+    } catch (error) {
+      res.redirect("/admin/dashboard");
+    }
+  }
+  static async updateSport(req, res) {
+    if (req.user.role != "ADMIN") {
+      return res.redirect("/player/dashboard");
+    }
+    const id = req.params.id;
+    console.log(id, "this is the sportId");
+
+    try {
+      const sport = await prisma.sport.findUnique({
+        where: {
+          id,
+        },
+      });
+
+      res.render("admin/update-sport", { user: req.user, sport, error: null });
+    } catch (error) {
+      res.render("admin/update-sport", { user: req.user, sport: null, error });
+    }
   }
 
   static createSportPage(req, res) {
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,11 +1,17 @@
-const express = require('express');
-const AdminController = require('../controllers/AdminController');
-const { authenticateToken } = require('../middleware/auth');
+const express = require("express");
+const AdminController = require("../controllers/AdminController");
+const { authenticateToken } = require("../middleware/auth");
 
 const router = express.Router();
 
-router.get('/dashboard', authenticateToken, AdminController.dashboard);
-router.get('/sports/new', authenticateToken, AdminController.createSportPage);
-router.post('/sports', authenticateToken, AdminController.createSport);
+router.get("/dashboard", authenticateToken, AdminController.dashboard);
+router.get("/sports/new", authenticateToken, AdminController.createSportPage);
+router.post("/sports", authenticateToken, AdminController.createSport);
+router.get("/sports/:id", authenticateToken, AdminController.updateSport);
+router.post(
+  "/update-sport/:id",
+  authenticateToken,
+  AdminController.updateSportsController
+);
 
 module.exports = router;
